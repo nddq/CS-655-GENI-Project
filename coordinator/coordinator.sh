2f04eaaae017def521171f513b44db87ab41c079
Quang Nguyen <nddq@bu.edu>
2021-12-08 00:34:47 -0500

    update scripts

--- coordinator/coordinator.sh
+++ coordinator/coordinator.sh
@@ -2,5 +2,4 @@
 sudo apt update  
 sudo apt install golang
 go get -u github.com/gorilla/mux
-cd CS-655-GENI-Project/coordinator
 go run main.go
--- worker/worker.sh
+++ worker/worker.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 sudo apt update  
 sudo apt install golang
-cd CS-655-GENI-Project/worker

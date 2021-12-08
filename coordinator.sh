924f73c0bc2c66755ea458b53233dc0ddf1cde0d
Quang Nguyen <nddq@bu.edu>
2021-12-08 00:37:14 -0500

    mvoe script outside

--- coordinator.sh
+++ coordinator.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
 sudo apt update  
 sudo apt install golang
 go get -u github.com/gorilla/mux
+git clone https://github.com/nddq/CS-655-GENI-Project.git
+cd CS-655-GENI-Project/coordinator
 go run main.go
